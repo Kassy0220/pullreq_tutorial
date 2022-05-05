7939d016f2e57d466f7667163abf7f439f25390f
github Kassy <github@shinbokutaro0220@gmail.com>
2022-05-05 10:30:26 +0900

    3番目のコメントを追加

--- sort.js
+++ sort.js
@@ -7,8 +7,9 @@ var sortNumber = function (number) {
 	});
 };
 
-// This is the first commit
-// This is the second commit
+// This is the first comment
+// This is the second comment
+// This is the third comment
 
 var number = [19, 3, 81, 1, 24, 21];
 sortNumber(number);
